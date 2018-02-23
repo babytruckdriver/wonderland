5bd083fcf4b68a853f2640597a7ad8e6d149a740
Samuel González Izquierdo <babytruckdriver@gmail.com>
2018-02-23 20:40:06 +0100

    Symbols and functions added

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -1,15 +1,27 @@
 (ns wonderland.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(foo "Name")
-
 ;; Symbols
+;; ==========
 (def developer "Alice") ;; Variable global
 (let [developer "Alice in Wonderland" ;; Variable con ámbito temporal
       rabbit "White Rabbit"]
   (println developer))
-(println developer)
+
+;; functions!
+;; ==========
+(defn follow-the-rabbit [] "Off we go!")
+(println (follow-the-rabbit))
+;; => Off we go!
+
+(defn sum [el1, el2]
+  (+ el1 el2))
+
+(println ( sum 2 3/2))
+;; => 7/2
+
+((fn [] (str "I'm an anonymous function!")))
+(#(str "I'm an anonymous function too!"))
+(#(str %1 ", " %2 ", I'm an anonymous function too!") "Samuel" "Cristina")
+;; => "I'm an anonymous function!"
+;; => "I'm an anonymous function too!"
+;; => "Samuel, Cristina, I'm an anonymous function too!"
